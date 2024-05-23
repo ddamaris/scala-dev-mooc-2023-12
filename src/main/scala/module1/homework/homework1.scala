0fce12a08421d356eeb79bf8e4e876df589ddd98
ddamaris <yzu@mail,ru>
2024-05-24 00:57:23 +0300

    c2

--- src/main/scala/module1/homework/homework1.scala
+++ src/main/scala/module1/homework/homework1.scala
@@ -2,20 +2,26 @@ package module1.homework
 
 
 import scala.util.Random
-/*
+
 class BallsExperiment {
 
-  def isFirstBlackSecondWhite(): Boolean = {
-    ???
+  private val box: Seq[Int] = List(0,1,0,1,0,1)
+  def isFirstBlackSecondWhite: Boolean = {
+    val idxOfFirstBall = Random.nextInt(box.size)
+    val isFirstBlack = box(idxOfFirstBall) == 0
+    val reducedBox = box.patch(idxOfFirstBall, Nil, 1)
+    val idxOfSecondBall = Random.nextInt(reducedBox.size)
+    val isSecondWhite = reducedBox(idxOfSecondBall) == 0
+    isFirstBlack && isSecondWhite
   }
 }
 
 object BallsTest {
   def main(args: Array[String]): Unit = {
     val count = 10000
-    val listOfExperiments: List[BallsExperiment] = ???
-    val countOfExperiments = ???
+    val listOfExperiments: List[BallsExperiment] = List.fill(count)(new BallsExperiment)
+    val countOfExperiments =  listOfExperiments.map(_.isFirstBlackSecondWhite)
     val countOfPositiveExperiments: Float = countOfExperiments.count(_ == true)
     println(countOfPositiveExperiments / count)
   }
-}*/
+}
